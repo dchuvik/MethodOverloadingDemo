e19f2daf6eb3f3a5a6b102a5ed9e06d60be30240
David Chuvik <dchuvik@gmail.com>
2023-02-27 09:35:00 -0500

    overloading example

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MethodOverloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonModel();
+            Console.ReadLine();
+        }
+    }
+}
+
+public class PersonModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public PersonModel()
+    {
+
+    }
+    public PersonModel(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+    public PersonModel(string firstName, string lastName, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email;
+    }
+}
